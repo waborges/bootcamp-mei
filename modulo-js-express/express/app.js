bec060a8cb37714f08ef95a6ad97d51d686d2ebd
Waborges <walace.borges@mercadolivre.com>
2022-07-22 20:32:23 -0300

    feat: implementa o funcionamento das rotas put,delete e get

--- modulo-js-express/express/app.js
+++ modulo-js-express/express/app.js
@@ -1,11 +1,54 @@
-const express = require('express');
+const express = require("express");
+let products = require("./products");
 
 const app = express();
 
-app.get('/', (_req,res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/products", (req, res) => {
+  const product = req.body;
+
+  if (!product) {
+    return res.status(400).send("Add the product correctly!");
+  }
+
+  const AddProduct = [...products, ...product];
+  products = AddProduct;
+  res.status(201).json(products);
+});
+
+app.put("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const content = req.body;
+
+  const product = products.find((product) => product.id === id);
+
+  if (!product) {
+    return res.status(400).json({ message: "Product not found" });
+  }
+
+  const newProducts = products.map((products) => {
+    if (products.id === id) return products = content;
+    return products;
+  });
+
+  return res.status(200).json(newProducts);
+});
+
+app.delete("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const product = products.filter((item) => item.id != id);
+  return res.status(200).json(product);
+});
+
+app.get("/products", (_req, res) => {
+  res.json(products);
 });
 
 app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+  console.log("Server running at http://localhost:3000");
 });
---
+++ modulo-js-express/express/products.js
@@ -0,0 +1,20 @@
+let products = [
+  {
+    id: 1,
+    name: "Macbook",
+    price: 1300,
+    quantity: 40,
+    colors: ["silver", "black", "white"],
+  },
+  {
+    id: 2,
+    name: "Iphone",
+    price: 1000,
+    quantity: 50,
+    colors: ["silver", "red", "white"],
+  },
+  { id: 3, name: "Pendrive", price: 10, quantity: 10, colors: [] },
+  { id: 4, name: "Headset", price: 50, quantity: 0, colors: ["black"] },
+];
+
+module.exports = products;
